21aa442430b2c78ac4f5f80ea951b7ff2c1bb40e
DanielBufon <97705860+DanielBufon@users.noreply.github.com>
2023-06-23 20:56:37 -0300

    Add files via upload

---
+++ dado.py
@@ -0,0 +1,44 @@
+import random
+
+resposta = "y"
+
+while resposta== "y":
+    no=random.randint(1,6)
+    if no== 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    if no== 2:
+        print("[-----]")
+        print("[ 0   ]")
+        print("[     ]")
+        print("[    0]")
+        print("[-----]")
+    if no== 3:
+        print("[-----]")
+        print("[  0  ]")
+        print("[  0  ]")
+        print("[  0  ]")
+        print("[-----]")
+    if no== 4:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[     ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    if no== 5:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[  0  ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    if no== 6:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[ 0 0 ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    
+    resposta = input("ypressione Y para jogar novamente e N para sair ")
